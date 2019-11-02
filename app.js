29f3639080ea5719cbb9bfcf60bdb11964eb2b59
Elias-san <elias.smartin@gmail.com>
2019-11-02 15:49:16 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,40 @@
+git status
+On branch master
+No commits yet
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        app.js
+nothing added to commit but untracked files present (use "git add" to track)
+git status
+On branch master
+No commits yet
+Changes to be committed:
+  (use "git rm --cached <file>..." to unstage)
+        new file:   app.js
+
+
+git commit -m "mi primer commit"
+[master (root-commit) a6d4018] mi primer commit
+ 1 file changed, 0 insertions(+), 0 deletions(-)
+ create mode 100644 app.js
+git status
+On branch master
+Your branch is based on 'origin/master', but the upstream is gone.
+  (use "git branch --unset-upstream" to fixup)
+nothing to commit, working tree clean
+git push origin master
+Enumerating objects: 3, done.
+Counting objects: 100% (3/3), done.
+Writing objects: 100% (3/3), 211 bytes | 42.00 KiB/s, done.
+Total 3 (delta 0), reused 0 (delta 0)
+To https://github.com/Elias-san/prueba2-node.git
+ * [new branch]      master -> master
+
+git status
+On branch master
+Your branch is up to date with 'origin/master'.
+nothing to commit, working tree clean
+
+
+git clone https://github.com/Elias-san/prueba2-node.git
+
